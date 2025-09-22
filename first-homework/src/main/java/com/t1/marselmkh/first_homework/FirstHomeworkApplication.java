ecb569e1e5792f2f6cd51f4be1022c74b6f7c2b8
Marsel <muhametshin.tech@gmail.com>
2025-09-23 00:32:19 +0300

    Initial commit

---
+++ first-homework/src/main/java/com/t1/marselmkh/first_homework/FirstHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.t1.marselmkh.first_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstHomeworkApplication.class, args);
+	}
+
+}
---
+++ first-homework/src/test/java/com/t1/marselmkh/first_homework/FirstHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1.marselmkh.first_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
